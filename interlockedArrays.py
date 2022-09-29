985a705723490404e30b2d58afed8fbd8368b00f
lucusiaaa <lucjapalkus@gmail.com>
2022-09-29 21:19:03 +0200

    Zadanie 'zazębione tablice'

---
+++ interlockedArrays.py
@@ -0,0 +1,22 @@
+# Return an array.
+# This function get string from user and change it into array.
+# User can pass any character from the keyboard.
+def getArr():
+    strin = input("Insert the string: ")
+    arr = list(strin)
+    return arr
+
+
+# This function takes two arrays as parameters
+# and interlocks their elements.
+# Return an array.
+def mixArrays(arr1, arr2):
+    arr3 = [None] * (min(len(arr1), len(arr2))) * 2
+    for i in range(min(len(arr1), len(arr2))):
+        arr3[i * 2] = arr1[i]
+        arr3[(i * 2) + 1] = arr2[i]
+    arr4 = arr1[min(len(arr1), len(arr2)):]
+    arr3 += arr4
+    return arr3
+
+print(mixArrays(getArr(), getArr()))
