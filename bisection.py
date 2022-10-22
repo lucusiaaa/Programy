3e404d2ae543b7b98df2eac45a7be9b5e6de7b46
lucusiaaa <lucjapalkus@gmail.com>
2022-10-22 13:05:09 +0200

    update bisection

--- bisection.py
+++ bisection.py
@@ -1,49 +1,33 @@
 # import timeit
+
 # Returns an integer.
 # This function take as parameter one sorted array which indexes are shifted by unknown amount.
 # Returns the starting index of array before shifting.
 def bisection(sortedArray):
+    if not sortedArray:
+        return None
     leftEnd = 0
     rightEnd = len(sortedArray) - 1
-    middle = int(len(sortedArray) / 2)
-
-    while sortedArray:
-        if sortedArray[middle] < sortedArray[middle - 1]:
-            return middle
-        if middle == leftEnd:
-            if sortedArray[middle] < sortedArray[rightEnd]:
-                return middle
-            else:
-                return rightEnd
-        if middle == rightEnd:
-            if sortedArray[middle] < sortedArray[leftEnd]:
-                return middle
-            else:
-                return leftEnd
-        if sortedArray[middle] < sortedArray[leftEnd] and sortedArray[middle] < sortedArray[rightEnd]:
-            rightEnd = middle - 1
-        if sortedArray[middle] > sortedArray[leftEnd] and sortedArray[middle] > sortedArray[rightEnd]:
+    middle = leftEnd + int((rightEnd - leftEnd) / 2)
+    while leftEnd < rightEnd:
+        if sortedArray[middle] > sortedArray[rightEnd]:
             leftEnd = middle + 1
-        if sortedArray[middle] > sortedArray[leftEnd] and sortedArray[middle] < sortedArray[rightEnd]:
-            rightEnd = middle - 1
+        else:
+            rightEnd = middle
         middle = leftEnd + int((rightEnd - leftEnd) / 2)
-
-
-def cheated(sortedArray):
-    minimum = min(sortedArray)
-    indexMin = sortedArray.index(minimum)
-    return indexMin
+    return middle
 
 
 def tests():
-    assert(bisection([1, 2, 3, 4, 5])) == 0
-    assert(bisection([2, 3, 4, 5, 1])) == 4
-    assert(bisection([1, 2, 3, 4, 5, 6])) == 0
-    assert(bisection([2, 3, 4, 5, 6, 1])) == 5
-    assert(bisection([4, 5, 1, 2, 3])) == 2
-    assert(bisection([])) == None
+    assert (bisection([1, 2, 3, 4, 5])) == 0
+    assert (bisection([2, 3, 4, 5, 1])) == 4
+    assert (bisection([1, 2, 3, 4, 5, 6])) == 0
+    assert (bisection([2, 3, 4, 5, 6, 1])) == 5
+    assert (bisection([4, 5, 1, 2, 3])) == 2
+    assert (bisection([])) is None
 
 
-tests()
-print("The index of the smallest element in the array: ", bisection([4, 5, 1, 2, 3]))
-# print("The index of the smallest element in the array: ", cheated([4, 5, 1, 2, 3]))
+# tests()
+print("The index of the smallest element in the array: ", bisection([2, 3, 4, 1]))
+
+# print("bisection runtime: ", timeit.timeit(bisection, number=1))
