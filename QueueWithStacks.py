fc3148dd279cb7cac1066bb583c03324d293fb4c
lucusiaaa <lucjapalkus@gmail.com>
2022-11-04 14:32:12 +0100

    update QueueWithStacks

--- QueueWithStacks.py
+++ QueueWithStacks.py
@@ -27,29 +27,45 @@ def main():
         options()
         option = input()
 
+        # 1 - add to queue
         if option == '1':
             myInput = input("Insert: ")
             stack1.append(myInput)
+        # 2 - remove item from queue
         elif option == '2':
-            while stack1:
-                stack2.append(stack1.pop())
+            if stack2 == []:
+                while stack1:
+                    stack2.append(stack1.pop())
             stack2.pop()
-            while stack2:
-                stack1.append(stack2.pop())
+        # 3 - check the last element in queue
         elif option == '3':
-            print(stack1[-1])
+            if stack1 != []:
+                print(stack1[-1])
+            else:
+                stack3 = stack2.copy()
+                stack3.reverse()
+                print(stack3[-1])
+        # 4 - print out the queue
         elif option == '4':
-            print(stack1)
+            if stack2!=[]:
+                stack3 = stack2.copy()
+                stack3.reverse()
+                print("Queue: ",stack3+stack1)
+            else:
+                print("Queue: ", stack1)
+        # 5 - check if queue is empty
         elif option == '5':
-            if stack1:
+            if stack1 or stack2:
                 print("Queue IS NOT empty")
             else:
                 print("Queue IS empty")
+        # 6 - quit
         elif option == '6':
             running = False
         else:
             print("There isn't such option!")
-
+        # print("stack1: ", stack1)
+        # print("stack2: ", stack2)
     return stack1
 
 
